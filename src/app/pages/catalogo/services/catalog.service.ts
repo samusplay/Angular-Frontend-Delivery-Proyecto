63c9c3baca9314b7014cee9115a6bd9829c49264
Brandon <brayeso2018@gmail.com>
2026-03-12 15:36:27 -0500

    botones descontar y reponer  con su logica en catalogo.ts la vista en cartalogo.html y la comunicacion con el backend en catalog.service.ts

--- src/app/pages/catalogo/services/catalog.service.ts
+++ src/app/pages/catalogo/services/catalog.service.ts
@@ -9,27 +9,53 @@ import { UpdateRequest } from '../models/UpdateRequest';
   providedIn: 'root',
 })
 export class CatalogService {
+
   private endpoint = 'api/catalog'
 
-  //constructor
-  constructor(private readonly backend: BackendService) { }
+  constructor(private readonly backend: BackendService) {}
 
-  //crear metodo
+  // crear
   CreateProduct(request: CreateRequest): Observable<CreateResponse> {
     const path = `${this.endpoint}/create`
     return this.backend.post<CreateResponse>(path, request)
-
   }
-  //actualizar
+
+  // actualizar
   UpdateProduct(id: number, request: UpdateRequest): Observable<CreateResponse> {
     const path = `${this.endpoint}/${id}/update`
     return this.backend.put<CreateResponse>(path, request)
   }
+
   // eliminar
   DeleteProduct(id: number): Observable<void> {
     const path = `${this.endpoint}/${id}/delete`
     return this.backend.delete<void>(path)
+  }
 
+  // obtener catalogo
+obtenerCatalogo() {
+  const path = `${this.endpoint}`
+  return this.backend.get<any[]>(path)
+}
+
+  // DESCONTAR STOCK
+  descontarStock(id: number, cantidad: number) {
+
+    const path = `${this.endpoint}/${id}/descontar`
+
+    return this.backend.post(path, {
+      cantidad: cantidad
+    })
+  }
+
+  // REPONER STOCK
+  reponerStock(id: number, cantidad: number) {
+
+    const path = `${this.endpoint}/${id}/reponer`
+
+    return this.backend.post(path, {
+      cantidad: cantidad
+    })
   }
 
 }
